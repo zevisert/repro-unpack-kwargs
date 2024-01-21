c855c18880a7ecba1f9015f69e1dfbf2c9e2d689
Zev Isert <dev@zevisert.ca>
2024-01-21 23:42:36 +0000

    style: organize imports

--- src/unpack_kwtypes/usage.py
+++ src/unpack_kwtypes/usage.py
@@ -1,4 +1,5 @@
 from typing import TYPE_CHECKING, Unpack
+
 from unpack_kwtypes.source import query_db, Movie
 
 
@@ -9,9 +10,9 @@ async def movie_titles_containing_word_the(
 
 
 if TYPE_CHECKING:
-    # src/unpack_kwtypes/usage.py:14: note: Revealed type is "def (**kwargs: Any) -> typing.Coroutine[Any, Any, builtins.list[TypedDict('unpack_kwtypes.source.Movie', {'title': builtins.str, 'year': builtins.int})]]"
+    # src/unpack_kwtypes/usage.py:15: note: Revealed type is "def (**kwargs: Any) -> typing.Coroutine[Any, Any, builtins.list[TypedDict('unpack_kwtypes.source.Movie', {'title': builtins.str, 'year': builtins.int})]]"
     # Expecting: "def (**kwargs: Unpack[TypedDict('unpack_kwtypes.source.QueryKeywordArgs', {'limit'?: Union[builtins.int, None], 'offset'?: Union[builtins.int, None], 'order'?: Union[Literal['ASC'], Literal['DESC']]})]) -> typing.Coroutine[Any, Any, builtins.list[TypedDict('unpack_kwtypes.source.Movie', {'title': builtins.str, 'year': builtins.int})]]"
     reveal_type(movie_titles_containing_word_the)
-    # src/unpack_kwtypes/usage.py:17: note: Revealed type is "def (*, limit: Union[builtins.int, None], offset: Union[builtins.int, None], order: Union[Literal['ASC'], Literal['DESC']]) -> TypedDict('unpack_kwtypes.source.QueryKeywordArgs', {'limit'?: Union[builtins.int, None], 'offset'?: Union[builtins.int, None], 'order'?: Union[Literal['ASC'], Literal['DESC']]})"
+    # src/unpack_kwtypes/usage.py:18: note: Revealed type is "def (*, limit: Union[builtins.int, None], offset: Union[builtins.int, None], order: Union[Literal['ASC'], Literal['DESC']]) -> TypedDict('unpack_kwtypes.source.QueryKeywordArgs', {'limit'?: Union[builtins.int, None], 'offset'?: Union[builtins.int, None], 'order'?: Union[Literal['ASC'], Literal['DESC']]})"
     # Expecting: "TypedDict('unpack_kwtypes.source.QueryKeywordArgs', {'limit'?: Union[builtins.int, None], 'offset'?: Union[builtins.int, None], 'order'?: Union[Literal['ASC'], Literal['DESC']]})"
     reveal_type(query_db.kwtypes)
